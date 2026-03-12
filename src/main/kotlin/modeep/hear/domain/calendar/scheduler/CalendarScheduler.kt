356be8546d192dcd437faa3f301ef67c63684ef3
kusuri12 <waterfail12@naver.com>
2026-03-12 10:33:36 +0900

    feat: calendar scheduler

---
+++ src/main/kotlin/modeep/hear/domain/calendar/scheduler/CalendarScheduler.kt
@@ -0,0 +1,48 @@
+package modeep.hear.domain.calendar.scheduler
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import modeep.hear.domain.calendar.service.CalendarService
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.LocalDate
+
+private val log = KotlinLogging.logger {}
+
+@Component
+class CalendarScheduler(
+    private val calendarService: CalendarService
+) {
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun initCalendarData() {
+        val targetYear = 2026
+        log.info { "Server init: Start to create calendar data about $targetYear year." }
+
+        saveYearlyCalendar(targetYear)
+
+        println("Complete $targetYear year calendar data creation.")
+    }
+
+    @Scheduled(cron = "0 0 2 1 12 ?", zone = "Asia/Seoul")
+    fun scheduleNextYearCalendar() {
+        val nextYear = LocalDate.now().year + 1
+        println("Regular Schedule: Generates calendar data for the $nextYear year in advance.")
+
+        saveYearlyCalendar(nextYear)
+    }
+
+    // 저장 로직
+    private fun saveYearlyCalendar(year: Int) {
+        (1..12).forEach { month ->
+            try {
+                calendarService.saveMonthCalendar(year, month)
+                Thread.sleep(100)  // API 과부하 방지
+            } catch (e: Exception) {
+                TODO("Error handling")
+                // println("Error: $year 년 $month 월 데이터 생성 중 오류 발생: ${e.message}")
+            }
+        }
+    }
+}
