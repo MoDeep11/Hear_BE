eed486d39b7aceedf929bf17ffc06347ff3e1438
kusuri12 <waterfail12@naver.com>
2026-03-16 23:15:06 +0900

    refactor: 어뷰징 방지 로직 추가

--- src/main/kotlin/modeep/hear/domain/auth/exception/AuthErrorCode.kt
+++ src/main/kotlin/modeep/hear/domain/auth/exception/AuthErrorCode.kt
@@ -12,12 +12,12 @@ enum class AuthErrorCode(
     PASSWORD_TOKEN_INVALID(HttpStatus.BAD_REQUEST, "AUTH_001", "이미 사용되었거나 만료된 토큰입니다."),
     EMAIL_ALREADY_VERIFIED(HttpStatus.CONFLICT, "AUTH_002", "이미 인증이 완료된 요청입니다."),
     VERIFICATION_TIMEOUT(HttpStatus.GONE, "AUTH_003", "인증 시간이 만료되었습니다."),
+    TOO_MANY_EMAIL_REQUESTS(HttpStatus.TOO_MANY_REQUESTS, "AUTH_004", "인증번호 요청은 1분에 한 번만 가능합니다."),
+    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "AUTH_005", "인증 코드가 일치하지 않습니다."),
 
-    INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "AUTH_004", "인증 코드가 일치하지 않습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_011", "유효하지 않은 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_012", "만료된 토큰입니다."),
+    LOGOUT_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_013", "로그아웃된 토큰입니다"),
 
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_005", "유효하지 않은 토큰입니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_006", "만료된 토큰입니다."),
-    LOGOUT_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_007", "로그아웃된 토큰입니다"),
-
-    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "AUTH_008", "비밀번호가 일치하지 않습니다.")
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "AUTH_021", "비밀번호가 일치하지 않습니다.")
 }
--- src/main/kotlin/modeep/hear/domain/auth/service/SendEmailAuthService.kt
+++ src/main/kotlin/modeep/hear/domain/auth/service/SendEmailAuthService.kt
@@ -1,20 +1,31 @@
 package modeep.hear.domain.auth.service
 
+import modeep.hear.domain.auth.exception.AuthErrorCode
 import modeep.hear.domain.auth.model.EmailVerification
 import modeep.hear.domain.auth.port.`in`.SendEmailUseCase
 import modeep.hear.domain.auth.port.out.EmailVerificationPort
 import modeep.hear.domain.auth.port.out.MailExternalPort
+import modeep.hear.global.error.exception.BusinessException
 import modeep.hear.global.util.VerificationCodeGenerator
 import modeep.hear.infrastructure.adapter.`in`.auth.dto.SendEmailRequest
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class SendEmailAuthService(
+    private val redisTemplate: StringRedisTemplate,  // TODO: 언젠가 변경
     private val codeGenerator: VerificationCodeGenerator,
     private val mailExternalPort: MailExternalPort,
     private val emailVerificationPort: EmailVerificationPort
 ) : SendEmailUseCase {
     override fun execute(request: SendEmailRequest) {
+        val limitKey = "AUTH:LIMIT:${request.email}"
+
+        if (redisTemplate.hasKey(limitKey) == true) {
+            throw BusinessException(AuthErrorCode.TOO_MANY_EMAIL_REQUESTS)
+        }
+
         val code = codeGenerator.generate()
 
         val emailVerification = EmailVerification(
@@ -24,5 +35,6 @@ class SendEmailAuthService(
         emailVerificationPort.save(emailVerification)
 
         mailExternalPort.send(emailVerification)
+        redisTemplate.opsForValue().set(limitKey, "sent", 60, TimeUnit.SECONDS)
     }
 }
