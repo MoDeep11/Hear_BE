7606628de3b8fbbb501050659f1124fe5e27d54c
kusuri12 <waterfail12@naver.com>
2026-03-17 19:46:16 +0900

    chore: 더이상 사용하지 않는 객체 Deprecated 처리

--- src/main/kotlin/modeep/hear/domain/auth/vo/ResetToken.kt
+++ src/main/kotlin/modeep/hear/domain/auth/vo/ResetToken.kt
@@ -1,5 +1,6 @@
 package modeep.hear.domain.auth.vo
 
+@Deprecated("Not used anymore")
 @JvmInline // 컴파일 시 Primitive 타입으로 취급. 즉 String 취급
 value class ResetToken(val value: String) {
     override fun toString(): String = "********" // 마스킹 처리
