e98e0c541ef5cc64b9a966890d6ed2b37cad2973
kusuri12 <waterfail12@naver.com>
2026-03-23 18:45:51 +0900

    fix: enum valid 검사 제외

--- src/main/kotlin/modeep/hear/infrastructure/adapter/in/auth/dto/request/SendEmailRequest.kt
+++ src/main/kotlin/modeep/hear/infrastructure/adapter/in/auth/dto/request/SendEmailRequest.kt
@@ -6,6 +6,5 @@ import modeep.hear.infrastructure.adapter.`in`.auth.dto.EmailRequestType
 data class SendEmailRequest(
     @field:NotBlank
     val email: String,
-    @field:NotBlank
     val type: EmailRequestType
 )
--- src/main/kotlin/modeep/hear/infrastructure/adapter/in/auth/dto/request/VerifyEmailRequest.kt
+++ src/main/kotlin/modeep/hear/infrastructure/adapter/in/auth/dto/request/VerifyEmailRequest.kt
@@ -6,7 +6,6 @@ import modeep.hear.infrastructure.adapter.`in`.auth.dto.EmailRequestType
 data class VerifyEmailRequest(
     @field:NotBlank
     val email: String,
-    @field:NotBlank
     val type: EmailRequestType,
     @field:NotBlank
     val code: String
