5b7aa0b1acf5198d45aac529cc7be7e524621944
kusuri12 <waterfail12@naver.com>
2026-03-28 21:38:39 +0900

    fix(chat): add composite adapter inherited message, chat port #42

---
+++ src/main/kotlin/modeep/hear/infrastructure/adapter/out/chat/MessageCompositeAdapter.kt
@@ -0,0 +1,29 @@
+package modeep.hear.infrastructure.adapter.out.chat
+
+import modeep.hear.domain.chat.model.Message
+import modeep.hear.domain.chat.port.out.MessagePort
+import modeep.hear.infrastructure.adapter.out.chat.external.MessageExternalAdapter
+import modeep.hear.infrastructure.adapter.out.chat.persistence.MessagePersistenceAdapter
+import org.springframework.stereotype.Component
+import java.util.UUID
+
+@Component
+class MessageCompositeAdapter (
+    private val persistenceAdapter: MessagePersistenceAdapter,
+    private val externalAdapter: MessageExternalAdapter
+) : MessagePort {
+
+    //--Persistence--//
+    override fun findAllByChatId(chatId: UUID): List<Message> =
+        persistenceAdapter.findAllByChatId(chatId)
+
+    override fun save(message: Message) =
+        persistenceAdapter.save(message)
+
+    //--External--//
+    override suspend fun sendMessage(
+        chatId: UUID,
+        message: Message
+    ): Message =
+        externalAdapter.sendMessage(chatId, message)
+}
