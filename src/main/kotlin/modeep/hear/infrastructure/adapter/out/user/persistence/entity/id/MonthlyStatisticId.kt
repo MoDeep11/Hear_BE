612b399e835f9dd4ade6a75c8729be3a05d735b2
kusuri12 <waterfail12@naver.com>
2026-03-31 19:04:35 +0900

    fix(user): update converter to targerYearMonth in MonthlyStatistic entity

--- src/main/kotlin/modeep/hear/infrastructure/adapter/out/user/persistence/entity/id/MonthlyStatisticId.kt
+++ src/main/kotlin/modeep/hear/infrastructure/adapter/out/user/persistence/entity/id/MonthlyStatisticId.kt
@@ -1,7 +1,9 @@
 package modeep.hear.infrastructure.adapter.out.user.persistence.entity.id
 
 import jakarta.persistence.Column
+import jakarta.persistence.Convert
 import jakarta.persistence.Embeddable
+import modeep.hear.global.converter.YearMonthConverter
 import java.io.Serializable
 import java.time.YearMonth
 import java.util.UUID
@@ -12,5 +14,6 @@ data class MonthlyStatisticId(
     val userId: UUID,
 
     @Column(name = "target_year_month")
+    @Convert(converter = YearMonthConverter::class)
     val targetYearMonth: YearMonth
 ) : Serializable
