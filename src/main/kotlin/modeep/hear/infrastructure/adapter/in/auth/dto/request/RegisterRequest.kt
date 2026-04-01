64134757a4876da78fef8e73998772efec333741
kusuri12 <waterfail12@naver.com>
2026-04-01 19:42:18 +0900

    fix(message): fix MessageConfig and update messages.properties #57

--- src/main/kotlin/modeep/hear/infrastructure/adapter/in/auth/dto/request/RegisterRequest.kt
+++ src/main/kotlin/modeep/hear/infrastructure/adapter/in/auth/dto/request/RegisterRequest.kt
@@ -8,12 +8,12 @@ import jakarta.validation.constraints.Size
 data class RegisterRequest(
     @field:NotBlank
     @field:Size(max = 100)
-    @field:Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @field:Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$")
     val email: String,
 
     @field:NotBlank
     @field:Size(min = 8, max = 100)
-    @field:Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\W)(?=\\S+$).+$", message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
+    @field:Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\W)(?=\\S+$).+$")
     val password: String,
 
     @field:JsonAlias("confirm_password")
--- src/main/kotlin/modeep/hear/infrastructure/config/env/MessageConfig.kt
+++ src/main/kotlin/modeep/hear/infrastructure/config/env/MessageConfig.kt
@@ -4,27 +4,32 @@ import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ResourceBundleMessageSource
+import org.springframework.validation.Validator
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver
 import java.util.Locale
 
 @Configuration
-class MessageConfig {
+class MessageConfig : WebMvcConfigurer {
+
     @Bean
     fun messageSource(): MessageSource {
         val messageSource = ResourceBundleMessageSource()
         messageSource.setBasenames("messages")
-        messageSource.setDefaultEncoding("UTF-8") // 한글 깨짐 방지
+        messageSource.setDefaultEncoding("UTF-8")
         return messageSource
     }
 
     @Bean
-    fun getValidator(): LocalValidatorFactoryBean {
+    fun localValidatorFactoryBean(): LocalValidatorFactoryBean {
         val bean = LocalValidatorFactoryBean()
         bean.setValidationMessageSource(messageSource())
         return bean
     }
+
+    override fun getValidator(): Validator = localValidatorFactoryBean()
 
     @Bean
     fun localeResolver(): LocaleResolver {
