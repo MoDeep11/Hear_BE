9497155baa686c7c24274725225fba7b8795b131
kusuri12 <waterfail12@naver.com>
2026-04-01 10:25:37 +0900

    fix(jackson): add primary annotation in objectmapper bean

--- src/main/kotlin/modeep/hear/infrastructure/config/env/JacksonConfig.kt
+++ src/main/kotlin/modeep/hear/infrastructure/config/env/JacksonConfig.kt
@@ -8,11 +8,13 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 class JacksonConfig {
 
     @Bean
+    @Primary
     fun objectMapper(): ObjectMapper {
         return jacksonObjectMapper()
             .registerModule(JavaTimeModule())
