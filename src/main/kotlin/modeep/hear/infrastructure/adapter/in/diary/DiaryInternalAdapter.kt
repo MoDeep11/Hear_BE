72c705b16fa5278fe1cdbb3ffbddf992f69d8355
kusuri12 <waterfail12@naver.com>
2026-04-07 00:12:45 +0900

    fix(diary): fix endpoint

--- src/main/kotlin/modeep/hear/infrastructure/adapter/in/diary/DiaryInternalAdapter.kt
+++ src/main/kotlin/modeep/hear/infrastructure/adapter/in/diary/DiaryInternalAdapter.kt
@@ -1,5 +1,6 @@
 package modeep.hear.infrastructure.adapter.`in`.diary
 
+import jakarta.validation.Valid
 import modeep.hear.domain.diary.port.`in`.CallbackGenerationDiaryImageUseCase
 import modeep.hear.domain.diary.port.`in`.CreateDiaryUseCase
 import modeep.hear.global.common.response.ApiResult
@@ -8,7 +9,10 @@ import modeep.hear.infrastructure.adapter.`in`.diary.dto.request.CallbackGenerat
 import modeep.hear.infrastructure.adapter.`in`.diary.dto.request.CreateDiaryRequest
 import modeep.hear.infrastructure.adapter.`in`.diary.dto.response.CreateDiaryResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
@@ -20,13 +24,18 @@ class DiaryInternalAdapter(
     private val callbackGenerationDiaryImageUseCase: CallbackGenerationDiaryImageUseCase
 ) : DiaryInternalApiDocument {
     @PostMapping
-    override suspend fun createDiary(request: CreateDiaryRequest): ResponseEntity<ApiResult<CreateDiaryResponse>> {
+    override suspend fun createDiary(
+        @RequestBody @Valid
+        request: CreateDiaryRequest
+    ): ResponseEntity<ApiResult<CreateDiaryResponse>> {
         val res = createDiaryUseCase.execute(request)
         return ResponseEntity.ok(ApiResult(data = res))
     }
 
+    @PatchMapping("/{diary_id}/images")
     override suspend fun callbackGenerationDiaryImage(
-        diaryId: UUID,
+        @PathVariable("diary_id") diaryId: UUID,
+        @RequestBody @Valid
         request: CallbackGenerationDiaryImageRequest
     ): ResponseEntity<ApiResult<Unit>> {
         callbackGenerationDiaryImageUseCase.execute(diaryId, request)
