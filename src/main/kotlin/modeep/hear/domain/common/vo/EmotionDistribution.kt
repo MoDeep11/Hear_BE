b2556db3fd150ef32ec1e2cbda276386a4bd4591
kusuri12 <waterfail12@naver.com>
2026-04-10 21:33:45 +0900

    fix(emotion): update data type to int

--- src/main/kotlin/modeep/hear/domain/common/vo/EmotionDistribution.kt
+++ src/main/kotlin/modeep/hear/domain/common/vo/EmotionDistribution.kt
@@ -1,24 +1,15 @@
 package modeep.hear.domain.common.vo
 
-import modeep.hear.global.util.EmotionUtil
-
 data class EmotionDistribution(
-    val values: Map<Emotion, Double>
+    val values: Map<Emotion, Int>
 ) {
-    init {
-        // require: IllegalArgumentException 반환
-        EmotionUtil.validateValues(values.values)
-    }
-
-    fun getScore(emotion: Emotion): Double = values.getOrDefault(emotion, 0.0)
+    fun getScore(emotion: Emotion): Int = values.getOrDefault(emotion, 0)
 
     companion object {
-        // KEYS.associateWith { VALUE }: Map 생성
-        fun empty() = EmotionDistribution(Emotion.entries.associateWith { 0.0 })
+        fun empty() = EmotionDistribution(Emotion.entries.associateWith { 0 })
 
         fun create(counts: Map<Emotion, Int>): EmotionDistribution {
-            val calculated = EmotionUtil.calculatePercentages(counts)
-            return EmotionDistribution(calculated)
+            return EmotionDistribution(counts)
         }
     }
 }
